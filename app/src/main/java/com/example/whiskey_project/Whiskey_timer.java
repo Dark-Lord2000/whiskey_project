c099147ae13e5e9de2fe9deedde4604cee86fc08
ml3558 <poke1111@my.yorku.ca>
2023-11-16 20:59:49 -0500

    added timer java

---
+++ app/src/main/java/com/example/whiskey_project/Whiskey_timer.java
@@ -0,0 +1,19 @@
+package com.example.whiskey_project;
+public class Whiskey_timer {
+    private long startTime = 0;
+    private long stopTime = 0;
+    private boolean running = false;
+
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        this.running = true;
+    }
+
+
+    public void stop() {
+        this.stopTime = System.currentTimeMillis();
+        this.running = false;
+    }
+
+}
