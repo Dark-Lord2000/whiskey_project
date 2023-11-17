f2787fb1932fa7c898a2e2321aabee0cbacbba75
ml3558 <poke1111@my.yorku.ca>
2023-11-17 15:00:21 -0500

    update timer backend

--- app/src/main/java/com/example/whiskey_project/Whiskey_timer.java
+++ app/src/main/java/com/example/whiskey_project/Whiskey_timer.java
@@ -4,16 +4,30 @@ public class Whiskey_timer {
     private long stopTime = 0;
     private boolean running = false;
 
-
+    // get start time
     public void start() {
         this.startTime = System.currentTimeMillis();
         this.running = true;
     }
 
-
+    // get stop time
     public void stop() {
         this.stopTime = System.currentTimeMillis();
         this.running = false;
+    }
+
+    // get elapsed time between start and end of seekbar interaction
+    public long elapsedTime(){
+        long elapsed;
+
+        if(running){
+            elapsed = (System.currentTimeMillis() - startTime) / 1000;
+        }
+        else {
+            elapsed = (stopTime - startTime) / 1000;
+        }
+
+        return elapsed;
     }
 
 }
