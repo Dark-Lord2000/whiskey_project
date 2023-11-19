432bb91407bfd51457fa95329c98302707eafc8c
miguelccruz <cruzmiguelbon@gmail.com>
2023-11-18 19:29:20 -0500

    fixed class naming

--- app/src/main/java/com/example/whiskey_project/Whiskey_setup.java
+++ app/src/main/java/com/example/whiskey_project/Whiskey_setup.java
@@ -7,7 +7,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Spinner;
 
-public class whiskey_setup extends Activity implements TextWatcher {
+public class Whiskey_setup extends Activity implements TextWatcher {
     String[] participantCode = {"P99", "P01", "P02", "P03", "P04", "P05", "P06", "P07", "P08", "P09", "P10"};
     String[] sessionCode = {"S99", "S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08"};
     String[] groupCode = {"G99", "G01", "G02"};
